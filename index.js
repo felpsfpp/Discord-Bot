6a4ca00d4edca875db8207405c53b1822ea6a209
Felipe P <felipepinheirocarneiro29@gmail.com>
2022-06-02 22:32:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
-const { Client, Intents } = require("discord.js");
+const { Client, Intents } = require('discord.js');
+const { prefix, token } = require('./config.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const play = require("./src/play.js");
-const getMusicFromURI = require("./src/extractURI.js");
 
-// carrega o token e prefixo
-const { token,prefix } = require('./config.json');
+client.once('ready', () => {
 
-client.once("ready", () => {
-
-    console.log(`Logged in as ${client.user.tag}`);
+	console.log(`Logged in as ${client.user.tag}`);
 
 });
 
-client.on('message', async (msg) => {
+client.on('messageCreate', async (msg) => {
+    if (msg.author.bot) return;
 
-    isCommand = msg.content.startsWith(prefix);
+    let text = msg.content;
 
-    if (!isCommand) return;
+    if (!text.startsWith(prefix)) return;
 
-    command = msg.content.split(prefix)[1].split(" ")[0];
-    args = msg.content.split(" ")[1];
-
-    console.log(`args = ${args}`); // debug
-
-    if (command == 'ping') {    
-        message.channel.send(`pong`);
-    }
-});
-
+    text = text.replace('!', '');
+    const args = text.split(" ");
+    switch (args[0].toLowerCase()) {
+		case 'ping':
+			msg.channel.send('Pong.');
+		return
+	}
+})
 
 client.login(token);
